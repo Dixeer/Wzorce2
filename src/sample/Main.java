81b4bf1c49e9eb975a44a0904fdf9d561a98162d
Dixeer <damian.zmuda22@gmail.com>
2016-05-28 20:11:49 +0200

    cos nie dziala

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,49 +1,28 @@
 package sample;
 
+import dao.JDBCTrainingDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Box;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
-import javax.xml.bind.SchemaOutputResolver;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Drawing Text");
-        Group root = new Group();
-        Scene scene = new Scene(root, 500, 300 , Color.WHITE);
-        int red = 30;
-        int green = 40;
-        int blue = 50;
-
-        Text text = new Text(250, 20, "Menu");
-        text.setWrappingWidth(200);
-        text.setTextAlignment(TextAlignment.CENTER);
-        Box box = new Box();
-
-
-
-        text.setFill(Color.rgb(red, green, blue, .999));
-        root.getChildren().add(text);
-
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        String a;
-
-    }
-
 
     public static void main(String[] args) {
+        JDBCTrainingDAO connect =new JDBCTrainingDAO();
+        connect.getConnection();
+        connect.select();
+        connect.closeConnection();
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Plan treningowy");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
